3694fd615316c902cbcf8cef7ca344977658bd2e
Fabijan Čorak <fc49106@fer.hr>
2017-12-22 09:08:49 +0100

    modify 22.py

--- 22.py
+++ 22.py
@@ -12,4 +12,31 @@ d = "u"
 cnt = 0
 for _ in range(10000):
     if grid[(cx, cy)] == "#":
-        
+        grid[(cx, cy)] = "."
+        if d == "u":
+            d = "r"
+            cy += 1
+        elif d == "r":
+            d = "d"
+            cx += 1
+        elif d == "d":
+            d = "l"
+            cy -= 1
+        elif d == "l":
+            d = "u"
+            cx -= 1
+    else:
+        grid[(cx, cy)] = "#"
+        cnt += 1
+        if d == "u":
+            d = "l"
+            cy -= 1
+        elif d == "l":
+            d = "d"
+            cx += 1
+        elif d == "d":
+            d = "r"
+            cy += 1
+        elif d == "d":
+            d = "u"
+            cx -= 1
